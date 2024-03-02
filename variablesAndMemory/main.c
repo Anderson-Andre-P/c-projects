c44de99eb78ca1c77f10d216bb56b0b491e1b0f5
Anderson-Andre-P <eleu.ander@gmail.com>
2024-03-02 17:30:55 -0300

    variables and memory

---
+++ variablesAndMemory/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int a = 10;
+	int b, c;
+	
+	printf("&a = %p, %d\n", &a, a);
+	printf("&b = %p, %d\n", &b, b);
+	printf("&c = %p, %d\n", &c, c);
+	
+	b = 20;
+	c = a + b;
+	
+	printf("&a = %p, %d\n", &a, a);
+	printf("&b = %p, %d\n", &b, b);
+	printf("&c = %p, %d\n", &c, c);
+	
+	return 0;
+}

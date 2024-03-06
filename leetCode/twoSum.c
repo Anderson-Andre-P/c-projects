b982ce1c93fdd5c5daa962a77829c3691104957d
Anderson-Andre-P <eleu.ander@gmail.com>
2024-03-06 17:33:14 -0300

    leetcode solution

---
+++ leetCode/twoSum.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int *arr;
+    int size;
+} Solution;
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    int *result = malloc(2 * sizeof(int));
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = 0; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target && i != j)
+            {
+                result[0] = i;
+                result[1] = j;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int target = 26;
+
+    int returnSize;
+
+    int *result = twoSum(nums, sizeof(nums) / sizeof(nums[0]), target, &returnSize);
+
+    if (result != NULL)
+    {
+        printf("[%d, %d]\n", result[0], result[1]);
+    }
+
+    free(result);
+
+    return 0;
+}
